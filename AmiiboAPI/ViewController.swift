22217e3bc5938d683895ac2085de013a7c1ad356
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-03-06 20:56:32 +0100

    Initial Commit

---
+++ AmiiboAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmiiboAPI
+//
+//  Created by Kresimir Ivanjko on 06.03.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
