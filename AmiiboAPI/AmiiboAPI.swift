41c291bf1b052e2df89cd3c0b10223942d22c749
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-03-06 21:18:23 +0100

    2

---
+++ AmiiboAPI/AmiiboAPI.swift
@@ -0,0 +1,81 @@
+//
+//  AmiiboAPI.swift
+//  AmiiboAPI
+//
+//  Created by Kresimir Ivanjko on 06.03.2023..
+//
+
+import Foundation
+
+
+// MARK: - Fetch data
+
+final class AmiiboAPI {
+    
+    static let shared = AmiiboAPI()
+    func fetchAmiiboList() {
+        let urlString = "https://amiiboapi.com/api/amiibo/"
+        let url = URL(string: urlString)!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, resp, error) in
+            
+            guard let data = data else {
+                print("data was nil")
+                return
+            }
+            
+            guard let amiiboList = try? JSONDecoder().decode(AmiiboList.self, from: data) else {
+                print("Couldnt decode JSON")
+                return
+            }
+            print(amiiboList.amiibo)
+            
+        }
+        task.resume()
+    }
+}
+
+
+// MARK: - Amiibo struct
+
+struct AmiiboList: Codable {
+    let amiibo: [Amiibo]
+}
+
+struct Amiibo: Codable {
+    let amiiboSeries: String
+    let character: String
+    let gameSeries: String
+    let head: String
+    let image: String
+    let name: String
+    let release: AmiiboRealase
+    let tail: String
+    
+}
+
+struct AmiiboRealase: Codable {
+    let au: String?
+    let eu: String?
+    let jp: String?
+    let na: String?
+}
+
+/*
+ "amiibo": [
+     {
+       "amiiboSeries": "Super Smash Bros.",
+       "character": "Mario",
+       "gameSeries": "Super Mario",
+       "head": "00000000",
+       "image": "https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_00000000-00000002.png",
+       "name": "Mario",
+       "release": {
+         "au": "2014-11-29",
+         "eu": "2014-11-28",
+         "jp": "2014-12-06",
+         "na": "2014-11-21"
+       },
+       "tail": "00000002",
+       "type": "Figure"
+ */
--- AmiiboAPI/ViewController.swift
+++ AmiiboAPI/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let tableView = UITableView()
+    var safeArea = UILayoutGuide()
+    let amiiboList = ["Zelda", "Link", "Navi"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        safeArea = view.layoutMarginsGuide
+        
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellid")
+        
+        setupView()
+        
+        AmiiboAPI.shared.fetchAmiiboList()
+        
+        
     }
-
+    
+    // MARK: - 1. SetupView
+    
+    func setupView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+    }
 
 }
 
+    // MARK: - 2. UITableViewDataSource
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return amiiboList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+        let name = amiiboList[indexPath.row]
+        cell.textLabel?.text = name
+        return cell
+    }
+    
+    
+}
